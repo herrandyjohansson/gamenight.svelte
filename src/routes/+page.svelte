53d8e4f4fff4ce145c831ab6646fdbc0b968f3cf
Andy Johansson <herrandyjohansson@gmail.com>
2024-11-05 14:54:19 +0100

    Add "no-cors" mode to fetch request headers in +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -16,6 +16,7 @@
           headers: {
             "Content-Type": "application/json",
             "Access-Control-Allow-Origin": "*",
+            mode: "no-cors",
           },
         }
       );
